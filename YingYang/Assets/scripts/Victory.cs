5357c240379cf9fec36dad63a884d65850c4c18a
Yanis Berkane <yanisberkane@Yaniss-MacBook-Air.local>
2023-05-27 20:26:29 +0200

    [FEAT] added pause menu, victory, loose scenes

---
+++ YingYang/Assets/scripts/Victory.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Victory : MonoBehaviour
+{
+    public void QuitGame() {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    public void MainMenuScene() {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
